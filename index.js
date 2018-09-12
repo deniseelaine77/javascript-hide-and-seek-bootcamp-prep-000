2a3a2778fabeac0efdd9e909557d14713333bddc
Denise Hastings <dehast59@gmail.com>
2018-09-12 19:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+// function increaseRankBy(n){
+//   //document.querySelectorAll(".ranked-list")[0].children[0].innerHTML += 1
+//   var children = 
+//   document.getElementsByClassName(".ranked-list).children.innerHTML;
+//   // for(i = 0; i< children.length; i++){
+//   // parseInt(innerHTML) += n;
+//   // }
+// }
+
+function deepestChild(){
+  var list = document.getElementById("grand-node");
+for (var i = 0; i < list.length; i++) {
+    querySelectorAll()
+    return list[i].innerHTML; //second console output
+}
+}
