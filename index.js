39404931b2cedee522246e3123125ee648d7ebee
Denise Hastings <dehast59@gmail.com>
2018-09-12 21:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function increaseRankBy(n) {
 
     //the tricky part; now for every child above, loop over all of the elements and change the innerHTML, using another for loop
     for (let j = 0, k = children.length; j < k; j++){
-      let newRank = n += parseInt(children[j].innerHTML); 
+      let newRank = parseInt(children[j].innerHTML) + n; 
 }
 }
 }
