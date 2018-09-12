04a3167725bd40fd42840c06f613dee7eb91eb61
Denise Hastings <dehast59@gmail.com>
2018-09-12 20:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,25 +6,7 @@ function nestedTarget() {
   return document.querySelector("#nested .target");
 }
 
-// function increaseRankBy(n){
-//   //document.querySelectorAll(".ranked-list")[0].children[0].innerHTML += 1
-//   var children = 
-//   document.getElementsByClassName(".ranked-list).children.innerHTML;
-//   // for(i = 0; i< children.length; i++){
-//   // parseInt(innerHTML) += n;
-//   // }
-// }
 
-//let node = document.getElementsByClassName(".ranked-list").children.innerHTML;
-//  let nextEl = node.children[i].inner.html;
-
-function increaseRankBy(n) {
-  const newRank = document
-  for(var i = 0; i < newRank.length; i++){
-    document.querySelectorAll(".ranked-list")[i].children[i].innerHTML;
-    return parseInt(newRank) + n
-}
-}
 
 //define a function deepestChild()
 //it should pull the most deeply nested child from div#grand-node
@@ -39,3 +21,25 @@ function deepestChild() {
  	}
   return node; //once the while loop is done, return node. 
 }
+
+
+
+// function increaseRankBy(n){
+//   //document.querySelectorAll(".ranked-list")[0].children[0].innerHTML += 1
+//   var children = 
+//   document.getElementsByClassName(".ranked-list).children.innerHTML;
+//   // for(i = 0; i< children.length; i++){
+//   // parseInt(innerHTML) += n;
+//   // }
+// }
+
+//let node = document.getElementsByClassName(".ranked-list").children.innerHTML;
+//  let nextEl = node.children[i].inner.html;
+
+// function increaseRankBy(n) {
+//   const newRank = document
+//   for(var i = 0; i < newRank.length; i++){
+//     document.querySelectorAll(".ranked-list")[i].children[i].innerHTML;
+//     return parseInt(newRank) + n
+// }
+// }
